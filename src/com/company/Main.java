0326e361dc9b9aa9f1a327ac85c4592f921d95c8
kingdomstay <kirill.troitskij@yandex.ru>
2020-01-20 17:26:41 +0300

    Добавлены циклы в валидации ходов игроков

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 
 public class Main {
 
+    // Statistics
     public static int scores1;
     public static int scores2;
-    public static int type;
+    public static String type;
     public static int rounds;
+
+    // Scanners
+    public static Scanner typeScanner = new Scanner(System.in);
+    public static Scanner p1Scanner = new Scanner(System.in);
+    public static Scanner p2Scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
         System.out.println("------------------ RPS Game ------------------");
@@ -16,26 +23,25 @@ public class Main {
     }
 
     private static void startGame(){
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Для начала игры выберите один из режимов игры: \n 1. Игра с другом \n 2. Игра с компьютером");
-        type = scanner.nextInt();
+        type = typeScanner.next();
         gameMode(type);
     }
 
-    private static void gameMode(int type){
+    private static void gameMode(String type){
         int player1;
         int player2;
         switch (type) {
-            case 1:
+            case "1":
                 System.out.println("Выбран режим игры с другом");
-                System.out.println("Для игры в RPS используйте значения \n R(камень), P(ножницы), S(бумага)");
+                System.out.println("Для игры в RPS используйте значения \n R(камень), P(бумага), S(ножницы)");
                 System.out.println("Ходит первый игрок:");
                 player1 = playerWalks(1);
                 System.out.println("Ходит второй игрок");
                 player2 = playerWalks(2);
                 checkResults(player1, player2);
                 break;
-            case 2:
+            case "2":
                 System.out.println("Выбран режим игры с компьютером");
                 System.out.println("Для игры в RPS используйте значения \n R(камень), P(ножницы), S(бумага)");
                 System.out.println("Ходит игрок:");
@@ -56,29 +62,41 @@ public class Main {
         String player2;
         int newPlayer2;
         int player0; // Компьютер
+
         Random random = new Random();
-        Scanner p1 = new Scanner(System.in);
-        Scanner p2 = new Scanner(System.in);
         switch (player){
             case 0:
                 player0 = random.nextInt(2);
                 player0++;
                 return player0;
             case 1:
-                player1 = p1.nextLine();
+                /*for(boolean validated = false; validated = false;){
+                    player1 = p1Scanner.nextLine();
+                    newPlayer1 = validateRequest(player1);
+                    if(newPlayer1 == 0){
+
+                    }
+                    else {
+                        return newPlayer1;
+                    }
+                }*/
+               /*
+                Рабочая версия
+
+                player1 = p1Scanner.nextLine();
                 newPlayer1 = validateRequest(player1);
                 if(newPlayer1 == 0){
-                    p2.nextLine();
+                    p1Scanner.nextLine();
                 }
                 else {
                     return newPlayer1;
-                }
+                }*/
                 break;
             case 2:
-                player2 = p2.nextLine();
+                player2 = p2Scanner.nextLine();
                 newPlayer2 = validateRequest(player2);
                 if(newPlayer2 == 0){
-                    p2.nextLine();
+                    p2Scanner.nextLine();
                 }
                 else {
                     return newPlayer2;
@@ -127,36 +145,36 @@ public class Main {
                 case 1:
                     switch (p2){
                         case 2:
-                            System.out.println("Победил второй игрок");
-                            scores2++;
-                            break;
-                        case 3:
                             System.out.println("Победил первый игрок");
                             scores1++;
+                            break;
+                        case 3:
+                            System.out.println("Победил второй игрок");
+                            scores2++;
                             break;
                     }
                 break;
                 case 2:
                     switch (p2){
                         case 1:
-                            System.out.println("Победил первый игрок");
-                            scores1++;
-                            break;
-                        case 3:
                             System.out.println("Победил второй игрок");
                             scores2++;
+                            break;
+                        case 3:
+                            System.out.println("Победил первый игрок");
+                            scores1++;
                             break;
                     }
                     break;
                 case 3:
                     switch (p2){
                         case 1:
-                            System.out.println("Победил второй игрок");
-                            scores2++;
-                            break;
-                        case 2:
                             System.out.println("Победил первый игрок");
                             scores1++;
+                            break;
+                        case 2:
+                            System.out.println("Победил второй игрок");
+                            scores2++;
                             break;
                     }
                     break;
@@ -191,7 +209,7 @@ public class Main {
         //float winrate2;
         System.out.println("--- Статистика игры ---");
         switch (type){
-            case 1:
+            case "1":
                 System.out.println("Вы играли с другом");
                 // winrate1 = scores1 / ((rounds - scores1) + scores1);
                 System.out.println("Счёт: \n " + "Первый игрок: "+ scores1 + " \n Второй игрок: " + scores2);
@@ -203,7 +221,7 @@ public class Main {
                     System.out.println("По итогам игры второй игрок обыграл первого игрока");
                 }
                 break;
-            case 2:
+            case "2":
                 System.out.println("Вы играли с компьютером");
                 // winrate2 = scores2 / ((rounds - scores2) + scores2);
                 System.out.println("Счёт: \n " + "Игрок: "+ scores1 + " \n Компьютер: " + scores2);
