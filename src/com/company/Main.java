b67c29e4ee01bc6e959158a272ef589ec8fc5bfb
Kirill Troitskij <kirill.troitskij@yandex.ru>
2020-02-11 00:21:29 +0300

    Добавлена локализация языков (RU, EN)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,11 @@
 package com.company;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -22,16 +28,116 @@ public class Main {
     public static Scanner replayScanner = new Scanner(System.in);
     public static Scanner replayGameScanner = new Scanner(System.in);
     public static Scanner resumeScanner = new Scanner(System.in);
+    public static Scanner selectLocaleScanner = new Scanner(System.in);
 
     // Localization
+    public static String locale;
+    public static HashMap<String, HashMap<String, String>> localeData = new HashMap();
+    public static HashMap<String, String> localeRU = new HashMap();
+    public static HashMap<String, String> localeEN = new HashMap();
     /*
     # ----
     # Soon
     # ----
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        // Init localization (import soon)
+        localeData.put("RU", localeRU);
+        localeData.put("EN", localeEN);
+        localeRU.put("startGame", "Для начала игры выберите один из режимов игры: \n 1. Игра с другом \n 2. Игра с компьютером");
+        localeEN.put("startGame", "To start the game, select one of the game modes:\n 1. Play with a friend\n 2. Game with a computer");
+
+        localeRU.put("selectedGM1", "Выбран режим игры с другом");
+        localeEN.put("selectedGM1", "Game mode with friend selected");
+        localeRU.put("selectedGM2", "Выбран режим игры с компьютером");
+        localeEN.put("selectedGM2", "Game mode with computer selected");
+
+        localeRU.put("replayGame", "Хотите сыграть ещё раз?");
+        localeEN.put("replayGame", "Want to play again?");
+        localeRU.put("gameOver", "Игра завершена");
+        localeEN.put("gameOver", "Game over");
+        localeRU.put("gameOverSelectAction", "Выполните одно из следующих действий\n 1. Вернуться к меню игры \n 2. Выйти из игры");
+        localeEN.put("gameOverSelectAction", "Perform one of the following actions\n 1. Return to the game menu\n 2. Quit the game");
+        localeRU.put("redirectToMenu", "Переход в меню..");
+        localeEN.put("redirectToMenu", "Go to menu..");
+
+        localeRU.put("enterName", "Введите имя для начала игры");
+        localeEN.put("enterName", "Enter name to start the game");
+
+        localeRU.put("p1", "Первый игрок");
+        localeEN.put("p1", "First player");
+        localeRU.put("p2", "Второй игрок");
+        localeEN.put("p2", "Second player");
+
+        localeRU.put("computer", "Компьютер");
+        localeEN.put("computer", "Computer");
+
+        localeRU.put("takePart", "В игре принимают участие два игрока - "+ p1_name + " и " + p2_name);
+        localeEN.put("takePart", "Two players take part in the game - " + p1_name + " and " + p2_name);
+
+        localeRU.put("p1Walks", "Ходит " + p1_name);
+        localeEN.put("p1Walks", p1_name + " walks");
+        localeRU.put("p2Walks", "Ходит " + p2_name);
+        localeEN.put("p2Walks", p2_name + " walks");
+
+        localeRU.put("FAQ", "Для игры в RPS используйте значения \n R(камень), P(бумага), S(ножницы)");
+        localeEN.put("FAQ", "To play RPS, use the values \n R (stone), P (paper), S (scissors)");
+
+        localeRU.put("FAQ", "Для игры в RPS используйте значения \n R(камень), P(бумага), S(ножницы)");
+        localeEN.put("FAQ", "To play RPS, use the values \n R (stone), P (paper), S (scissors)");
+
+        localeRU.put("FAQ", "Для игры в RPS используйте значения \n R(камень), P(бумага), S(ножницы)");
+        localeEN.put("FAQ", "To play RPS, use the values \n R (stone), P (paper), S (scissors)");
+
+        localeRU.put("enterToContinue", "Введите любой символ чтобы продолжить..");
+        localeEN.put("enterToContinue", "Enter any character to continue ..");
+
+        // Variations
+
+        localeRU.put("draw", "Ничья");
+        localeEN.put("draw", "Draw");
+        localeRU.put("win1", "Победил " + p1_name);
+        localeEN.put("win1", p1_name + " won");
+        localeRU.put("win2", "Победил " + p2_name);
+        localeEN.put("win2", p2_name + " won");
+
+        // Errors
+        localeRU.put("incorrectValue", "\u001B[31mВведено некорректное значение, попробуйте ещё раз\u001B[0m");
+        localeEN.put("incorrectValue", "\u001B[31mIncorrect value entered, try again\u001B[0m");
+
+        localeRU.put("emptyValue", "\u001B[31mВведено пустое значение\u001B[0m");
+        localeEN.put("emptyValue", "\u001B[31mNull value entered\u001B[0m");
+
+        localeRU.put("doesNotMatchValue", "\u001B[31mВведите имя, не совпадающее с вашим противником\u001B[0m");
+        localeEN.put("doesNotMatchValue", "\u001B[31mEnter a name that does not match your opponent\u001B[0m");
+
+        localeRU.put("errorAndFAQ", "\u001B[31mОшибка \nДля игры в RPS используйте значения \n R(камень), P(ножницы), S(бумага)\u001B[0m");
+        localeEN.put("errorAndFAQ", "\u001B[31mError \nTo play RPS, use the values \n R (stone), P (paper), S (scissors)\u001B[0m");
+        localeRU.put("replayGameFAQ", "\u001B[31mОшибка \n Используйте слова да или нет для выбора ответа\u001B[0m");
+        localeEN.put("replayGameFAQ", "\u001B[31mError \n Use yes or no to select an answer\u001B[0m");
+
+        localeRU.put("error", "\u001B[31mОшибка\u001B[0m");
+        localeEN.put("error", "\u001B[31mError\u001B[0m");
+
+        // Statistic
+
+        localeRU.put("statistic", "----- Статистика игры -----");
+        localeEN.put("statistic", "----- Game stats -----");
+        localeRU.put("youPlayedWithComputer", "Вы играли с компьютером");
+        localeEN.put("youPlayedWithComputer", "You played with the computer");
+        localeRU.put("score", "Счёт: \n " + p1_name + ": "+ scores1 + " \n " + p2_name + ": " + scores2);
+        localeEN.put("score", "Score: \n " + p1_name + ": "+ scores1 + " \n " + p2_name + ": " + scores2);
+        localeRU.put("gamesPlayed", "Сыграно партий: " + rounds);
+        localeEN.put("gamesPlayed", "Games played: " + rounds);
+        localeRU.put("endGameP1", "По итогам игры выиграл " + p1_name);
+        localeEN.put("endGameP1", "At the end of the game, " + p1_name + " won");
+        localeRU.put("endGameP2", "По итогам игры выиграл " + p2_name);
+        localeEN.put("endGameP2", "At the end of the game, " + p2_name + " won");
+
+
         System.out.println("---------------- ✊ ✌ ✋ Game ----------------");
+        locale = selectLocale();
         startGame();
     }
 
@@ -43,9 +149,33 @@ public class Main {
         p2_name = "player2";
         type = null;
         rounds = 0;
-        System.out.println("Для начала игры выберите один из режимов игры: \n 1. Игра с другом \n 2. Игра с компьютером");
+        sendText("startGame");
         type = typeScanner.next();
         gameMode(type);
+    }
+
+    private static void sendText(String key) {
+        System.out.println(localeData.get(locale).get(key));
+    }
+
+    private static String selectLocale (){
+        boolean valide;
+        do {
+            System.out.println("Select a game language to continue:\n 1. Russian(Русский) \n 2. English");
+            switch (selectLocaleScanner.nextLine()){
+                case "1":
+                    valide = true;
+                    return "RU";
+                case "2":
+                    valide = true;
+                    return "EN";
+                default:
+                    sendText("error");
+                    valide = false;
+                    break;
+            }
+        } while (valide = true);
+        return "";
     }
 
     private static void gameMode(String type){
@@ -53,39 +183,59 @@ public class Main {
         int player2;
         switch (type) {
             case "1":
-                System.out.println("Выбран режим игры с другом");
+                sendText("selectedGM1");
                 if(!nameInit){
-                    System.out.println("Для старта игры необходимо ввести ваши имена");
-                    System.out.println("Первый игрок");
+                    sendText("enterName");
+                    sendText("p1");
                     p1_name = setName();
-                    System.out.println("Второй игрок");
+                    sendText("p2");
                     p2_name = setName();
-                    System.out.println("В игре принимают участие два игрока - "+ p1_name + " и " + p2_name);
+                    // Update players
+                    localeRU.put("takePart", "В игре принимают участие два игрока - "+ p1_name + " и " + p2_name);
+                    localeEN.put("takePart", "Two players take part in the game - " + p1_name + " and " + p2_name);
+                    localeRU.put("p1Walks", "Ходит " + p1_name);
+                    localeEN.put("p1Walks", p1_name + " walks");
+                    localeRU.put("p2Walks", "Ходит " + p2_name);
+                    localeEN.put("p2Walks", p2_name + " walks");
+                    localeRU.put("win1", "Победил " + p1_name);
+                    localeEN.put("win1", p1_name + " won");
+                    localeRU.put("win2", "Победил " + p2_name);
+                    localeEN.put("win2", p2_name + " won");
+                    sendText("takePart");
                     nameInit = true;
                 }
-                System.out.println("Для игры в RPS используйте значения \n R(камень), P(бумага), S(ножницы)");
-                System.out.println("Ходит "+ p1_name +":");
+                sendText("FAQ");
+                sendText("p1Walks");
                 player1 = playerWalks(1);
-                System.out.println("Ходит "+ p2_name +":");
+                sendText("p2Walks");
                 player2 = playerWalks(2);
                 checkResults(player1, player2);
                 break;
             case "2":
-                System.out.println("Выбран режим игры с компьютером");
+                sendText("selectedGM2");
                 if(!nameInit){
-                    System.out.println("Для старта игры необходимо ввести ваше имя");
+                    sendText("enterName");
                     p1_name = setName();
-                    p2_name = "Компьютер";
+                    p2_name = localeData.get(locale).get("computer");
+                    // Update players
+                    localeRU.put("p1Walks", "Ходит " + p1_name);
+                    localeEN.put("p1Walks", p1_name + " walks");
+                    localeRU.put("p2Walks", "Ходит " + p2_name);
+                    localeEN.put("p2Walks", p2_name + " walks");
+                    localeRU.put("win1", "Победил " + p1_name);
+                    localeEN.put("win1", p1_name + " won");
+                    localeRU.put("win2", "Победил " + p2_name);
+                    localeEN.put("win2", p2_name + " won");
                     nameInit = true;
                 }
-                System.out.println("Для игры в RPS используйте значения \n R(камень), P(бумага), S(ножницы)");
-                System.out.println("Ходит "+ p1_name +":");
+                sendText("FAQ");
+                sendText("p1Walks");
                 player1 = playerWalks(1);
-                System.out.println("Ходит компьютер");
+                sendText("p2Walks");
                 checkResults(player1, playerWalks(0));
                 break;
             default:
-                System.out.println("Введено некорректное значение, попробуйте ещё раз");
+                sendText("incorrectValue");
                 startGame();
                 break;
         }
@@ -95,15 +245,14 @@ public class Main {
         boolean valide;
 
         do {
-            System.out.println("Введите имя");
             String name = nameScanner.nextLine();
             if(name.equals("")){
-                System.out.println("Введено пустое значение");
+                sendText("emptyValue");
                 valide = false;
             }
             else {
                 if(p1_name.equals(name) || p2_name.equals(name)){
-                    System.out.println("Введите имя, не совпадающее с вашим противником");
+                    sendText("doesNotMatchValue");
                     valide = false;
                 }
                 else {
@@ -177,7 +326,7 @@ public class Main {
                 return 3;
 
             default:
-                System.out.println("Ошибка \n Для игры в RPS используйте значения \n R(камень), P(ножницы), S(бумага)");
+                sendText("errorAndFAQ");
                 return 0;
         }
     }
@@ -189,7 +338,7 @@ public class Main {
         *  return 2 | ничья
         * */
         if(p1 == p2){
-            System.out.println("Ничья");
+            sendText("draw");
             scores1++;
             scores2++;
         }
@@ -198,11 +347,11 @@ public class Main {
                 case 1:
                     switch (p2){
                         case 2:
-                            System.out.println("Победил "+ p1_name);
+                            sendText("win1");
                             scores1++;
                             break;
                         case 3:
-                            System.out.println("Победил "+ p2_name);
+                            sendText("win2");
                             scores2++;
                             break;
                     }
@@ -210,11 +359,11 @@ public class Main {
                 case 2:
                     switch (p2){
                         case 1:
-                            System.out.println("Победил "+ p2_name);
+                            sendText("win2");
                             scores2++;
                             break;
                         case 3:
-                            System.out.println("Победил "+ p1_name);
+                            sendText("win1");
                             scores1++;
                             break;
                     }
@@ -222,11 +371,11 @@ public class Main {
                 case 3:
                     switch (p2){
                         case 1:
-                            System.out.println("Победил "+ p1_name);
+                            sendText("win1");
                             scores1++;
                             break;
                         case 2:
-                            System.out.println("Победил "+ p2_name);
+                            sendText("win2");
                             scores2++;
                             break;
                     }
@@ -253,7 +402,7 @@ public class Main {
     private static void replay() {
         String replay;
         boolean valide;
-        System.out.println("Хотите сыграть ещё раз?");
+        sendText("replayGame");
         do {
             replay = replayScanner.nextLine();
             switch (replay.toLowerCase()) {
@@ -272,7 +421,7 @@ public class Main {
                     valide = true;
                     break;
                 default:
-                    System.out.println("Так всё таки да или нет?");
+                    sendText("replayGameFAQ");
                     valide = false;
                     break;
             }
@@ -299,44 +448,53 @@ public class Main {
         ║ По итогам игры выиграл первый игрок ║
         ╚═════════════════════════════════════╝
         */
-        System.out.println("--- Статистика игры ---");
+        sendText("statistic");
+        // Update Variables
+        localeRU.put("gamesPlayed", "Сыграно партий: " + rounds);
+        localeEN.put("gamesPlayed", "Games played: " + rounds);
+        localeRU.put("score", "Счёт: \n " + p1_name + ": "+ scores1 + " \n " + p2_name + ": " + scores2);
+        localeEN.put("score", "Score: \n " + p1_name + ": "+ scores1 + " \n " + p2_name + ": " + scores2);
+        localeRU.put("endGameP1", "По итогам игры выиграл " + p1_name);
+        localeEN.put("endGameP1", "At the end of the game, " + p1_name + " won");
+        localeRU.put("endGameP2", "По итогам игры выиграл " + p2_name);
+        localeEN.put("endGameP2", "At the end of the game, " + p2_name + " won");
+
         switch (type){
             case "1":
-                System.out.println("Вы играли с другом");
                 // winrate1 = scores1 / ((rounds - scores1) + scores1);
-                System.out.println("Счёт: \n " + p1_name + ": "+ scores1 + " \n " + p2_name + ": "+ scores2);
-                System.out.println("Сыграно партий: " + rounds);
-                if(scores1 == scores2){
-                    System.out.println("Победила дружба");
+                sendText("score");
+                sendText("gamesPlayed");
+                if(scores1 == scores2) {
+                    sendText("draw");
                 }
                 else {
                     if(scores1 > scores2) {
-                        System.out.println("По итогам игры выиграл "+ p1_name);
+                        sendText("endGameP1");
                     }
                     else {
-                        System.out.println("По итогам игры выиграл "+ p2_name);
+                        sendText("endGameP2");
                     }
                 }
                 break;
             case "2":
-                System.out.println("Вы играли с компьютером");
+                sendText("youPlayedWithComputer");
                 // winrate2 = scores2 / ((rounds - scores2) + scores2);
-                System.out.println("Счёт: \n " + p1_name + ": "+ scores1 + " \n Компьютер: " + scores2);
-                System.out.println("Сыграно партий: " + rounds);
-                if(scores1 == scores2){
-                    System.out.println("Победила дружба");
+                sendText("score");
+                sendText("gamesPlayed");
+                if(scores1 == scores2) {
+                    sendText("draw");
                 }
                 else {
                     if(scores1 > scores2) {
-                        System.out.println("По итогам игры "+ p1_name + " обыграл компьютер");
+                        sendText("endGameP1");
                     }
                     else {
-                        System.out.println(p1_name + " проиграл компьютеру");
+                        sendText("endGameP2");
                     }
                 }
                 break;
         }
-        System.out.println("Введите любой символ чтобы продолжить..");
+        sendText("enterToContinue");
         String resume = resumeScanner.nextLine();
         replayGame();
     }
@@ -344,14 +502,14 @@ public class Main {
         boolean valide;
         String type;
 
-        System.out.println("Игра завершена");
+        sendText("gameOver");
         do {
-            System.out.println("Выполните одно из следующих действий\n 1. Вернуться к меню игры \n 2. Выйти из игры");
+            sendText("gameOverSelectAction");
             type = replayGameScanner.nextLine();
             switch (type) {
                 case "1":
                     valide = true;
-                    System.out.println("Переход в меню..");
+                    sendText("redirectToMenu");
                     startGame();
                     break;
                 case "2":
@@ -362,7 +520,7 @@ public class Main {
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Ошибка");
+                    sendText("error");
                     valide = false;
                     break;
             }
